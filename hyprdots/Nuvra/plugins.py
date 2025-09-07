239a955506e6279c6613bc57ce20ec6402c0315f
voitelo <bery7yahoo.de@gmail.com>
2025-09-07 16:53:58 +0200

    Add files via upload

---
+++ hyprdots/Nuvra/plugins.py
@@ -0,0 +1,13 @@
+# Minimal plugin hooks template
+class Plugin:
+    def on_tab_create(self, tab):
+        pass
+
+    def on_load(self, url):
+        pass
+
+    def on_search(self, query):
+        pass
+
+    def on_workspace_switch(self, workspace):
+        pass
