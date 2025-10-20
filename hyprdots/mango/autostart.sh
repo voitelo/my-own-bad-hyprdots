31567b142af121afdc41cb7da4096ca3c8db9151
voitelo <bery7yahoo.de@gmail.com>
2025-10-20 13:07:33 +0200

    Add files via upload

---
+++ hyprdots/mango/autostart.sh
@@ -0,0 +1,5 @@
+quickshell >/dev/null 2>&1 &
+sleep 3 && swaybg -i wallpapers/cartooney-nature.png >/dev/null 2>&1 &
+Scripts/battery-warning.sh >/dev/null 2>&1 &
+hypridle & >/dev/null 2>&1 &
+notify-send -t 4000 "you stupid ass bitch, go back to windows motherfucker" >/dev/null 2>&1 &
